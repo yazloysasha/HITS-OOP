daa5e310384fc38c6db18582d5d203017b89d63e
yazloysasha <yazloysasha@gmail.com>
2024-04-26 17:43:39 +0700

    Добавлена проверка корректности закреплённого животного

--- src/people/Employee.kt
+++ src/people/Employee.kt
@@ -11,7 +11,15 @@ class Employee(
     initAnimal: String
 ): Person(initName, initSex) {
     var job = initJob // Должность
-    var animal = initAnimal // Животное, закреплённое за сотрудником
+    var animal = parseAnimal(initAnimal) // Животное, закреплённое за сотрудником
+        set(value) {
+            field = parseAnimal(value)
+        }
+
+    // Получить животного по его типу
+    private fun parseAnimal(type: String): String {
+        return if (type == "parrot" || type == "wolf" || type == "lion") type else "NONE"
+    }
 
     // Редактировать сотрудника
     fun edit(newName: String, newJob: String, newAnimal: String) {
@@ -22,6 +30,6 @@ class Employee(
 
     // Проверить статус сотрудника
     fun checkStatus() {
-        println("[Employee] Name: $name | Sex: $sex | Job: $job")
+        println("[Employee] Name: $name | Sex: $sex | Job: $job | Animal: $animal")
     }
 }
